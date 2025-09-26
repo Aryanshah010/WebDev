0774f5b85819ca8b78e7f1400a658f9ebc8e5bfb
Aryanshah <aryanshah@Aryanshahs-MacBook-Pro.local>
2025-09-27 00:10:31 +0545

    Basic DOM Manipulation completed

---
+++ javascript/domBasic.js
@@ -0,0 +1,96 @@
+// let t=document.querySelector("h1")
+// t.textContent="hello Aryan";
+// t.innerHTML="<i>hello</i> Aryan";
+
+// let anchor=document.querySelector("a")
+// anchor.href="https://www.apple.com";
+// anchor.setAttribute("href","https://www.apple.com");
+// console.log(anchor.getAttribute("href"));
+// anchor.removeAttribute("href");
+
+// let h2=document.createElement("h2");
+// h2.innerText="Hey i am new element";
+// // document.body.prepend(h2);
+// document.body.append(h2);
+
+// let h1=document.createElement("h1");
+// h1.innerText="I am h1 5";
+// document.querySelector("div").appendChild(h1)
+// h1.remove()
+// document.querySelector("div").prepend(h1)
+
+// let d=document.querySelector("div")
+// d.style.backgroundColor="blue";
+// d.style.color="orange";
+// d.style.textAlign="center";
+// let c=document.createElement("h1")
+// c.innerText="I am h1 5";
+// d.prepend(c)
+// d.classList.toggle("wrapper")
+// d.classList.remove("wrapper")
+// d.removeAttribute("class")
+
+// let div=document.createElement("div")
+// div.classList.add("wrapper")
+// div.style.backgroundColor="pink";
+// div.style.color="blue";
+// document.body.appendChild(div)
+
+// for (let index = 1; index <= 5; index++) {
+//   let p=document.createElement("P")
+//   p.innerText="adksfhasudofh";
+//   div.appendChild(p);
+// }
+
+// let buy=document.querySelectorAll(".buy-now");
+
+// buy.forEach(b=>{
+//   b.style.backgroundColor="red";
+// });
+// console.dir(buy);
+
+// let h=document.querySelector("#mainHeading");
+// h.innerText="Welcome to AryanSchool";
+
+// let list=document.querySelectorAll("li");
+// list.forEach(l=>{
+//    console.log(l.textContent);
+// });
+
+// let p=document.querySelector("P")
+// p.innerHTML="<b>Updated</b> by javascript"
+
+// let a=document.querySelector("a");
+// a.href="https://www.sheryians.com";
+
+// let d=document.querySelector("div")
+// d.setAttribute("title","helloworld")
+
+// let b=document.querySelector("button")
+// b.removeAttribute("disabled")
+
+// let l=document.querySelector("ul");
+// let newl=document.createElement("li");
+// newl.textContent="New Task";
+// l.appendChild(newl);
+
+// let i = document.createElement("img");
+// i.setAttribute(
+//   "src",
+//   "https://images.unsplash.com/photo-1751716534754-e4eb69f18e90?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyfHx8ZW58MHx8fHx8"
+// );
+// document.querySelector("div").prepend(i);
+//  let ul=document.querySelector("ul");
+//  let l=document.querySelector("li");
+//  ul.removeChild(l)
+
+// let ul=document.querySelector("ul");
+// let list=document.querySelectorAll("ul li:nth-child(2n)");
+// list.forEach(elem=>{
+//   elem.classList.add("highlight");
+// })
+
+// let par=document.querySelectorAll("p")
+// par.forEach(pix=>{
+//   pix.style.fontSize="24px";
+// })
